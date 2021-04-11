32007c2e34a45044b913710e0f2acb4d647fb69f
LucasMaiorini <lucasmaiorini@gmai.com>
2021-04-11 17:36:36 -0300

    Events Interface upgraded. Added a filter system.

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -1,25 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventosComponent,
-    PalestrantesComponent
+    PalestrantesComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    CollapseModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,17 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public events: any = [];
+  public filteredEvents: any = [];
+  showingImage = true;
+  private _filtersListBy = '';
+
+  public get filtersListBy(): string {
+    return this._filtersListBy;
+  }
+
+  public set filtersListBy(value: string) {
+    this._filtersListBy = value;
+    this.filteredEvents = this.filtersListBy ? this.eventsFiltered(this._filtersListBy) : this.events;
+  }
+
   constructor(public http: HttpClient) { }
 
   ngOnInit(): void {
     this.getEvents();
   }
 
-  public getEvents(): void {
+  getEvents(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.events = response;
+        this.filteredEvents = response
+      },
       error => console.log(error)
     );
+  }
+  eventsFiltered(filtersListBy: string): any {
+    filtersListBy = filtersListBy.toLocaleLowerCase();
+    return this.filteredEvents.filter(
+      (event: any) => event.theme.toLocaleLowerCase().indexOf(filtersListBy) !== -1 ||
+        event.location.toLocaleLowerCase().indexOf(filtersListBy) !== -1
+    );
+  }
+
+  showImage(): void {
+    this.showingImage = !this.showingImage;
   }
 }
